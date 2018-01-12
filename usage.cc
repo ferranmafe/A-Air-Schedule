d0ced9b14369c614446f573cbe76aaf2e1283086
Ferran Martínez <ferran.mafe@gmail.com>
2018-01-12 22:48:50 +0100

    Added usage

---
+++ usage.cc
@@ -0,0 +1,9 @@
+void usage() {
+  cout << "./program  [Algoritmo de Max Flow] [Versión] [Fichero de entrada]" << endl;
+  cout << "Programa que calcula, dados unos vuelos de entrada, el schedule que requiere el mínimo de pilotos para llevarse a cabo" << endl;
+  exit(1);
+}
+
+int int main(int argc, char* argv[] ) {
+  if (argc != 3) usage();
+}
